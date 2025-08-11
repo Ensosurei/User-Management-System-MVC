bbd84d44b6dbba7d48be4853770aee679b13f8c1
Mercedes Ramos <mercedesramosvega13@gmail.com>
2025-08-11 13:18:40 -0700

    Se creo la estructura del proyecto

---
+++ src/Controlador/Controlador.java
@@ -0,0 +1,19 @@
+
+package Controlador;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import Modelo.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Controlador implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/Modelo/ManejadorDB.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author ensos
+ */
+import java.sql.*;
+
+public abstract class ManejadorDB {
+    protected Connection conexion;
+    protected PreparedStatement pstm;
+    protected ResultSet registros;
+    protected String sqlConsulta;
+    
+    //CONSTANTES
+    final static String DATABASE="sistema";
+    final static String IP_SERVER="localhost";
+    final static String USER="root";
+    final static String PASSWORD="1234";
+    final static String URL ="jdbc:mysql://"+ IP_SERVER + ":3306/" + DATABASE;
+    
+    //METODOS DE PERSISTENCIA
+    public abstract int crearTabla();
+    public abstract int registrar(Object obj);
+    public abstract int actualizar(Object obj);
+    public abstract int activar(Object obj);
+    public abstract int desactivar(Object obj);
+    
+    //METODOS DE PROYECCION
+    public abstract Object consultar(Object obj);
+    public abstract ResultSet todos(int status);
+    public abstract int contarRegistros(int status);
+  
+    public boolean abrir(){
+        boolean exito= false;
+        
+        try{
+            conexion=DriverManager.getConnection(URL, USER, PASSWORD);
+            if(conexion!=null)exito=true;
+        }catch(SQLException e){
+            System.err.println("Surgio un error al abrir la base de datos "+e.getMessage());
+        }
+        return exito;
+    }
+    
+    public void cerrar(){
+        try{
+            if(!conexion.isClosed()) conexion.close();
+        }catch(SQLException e){
+            System.err.println("No se pudo cerrar la base de datos  "+e.getMessage());
+        }
+        
+        
+    }
+}
